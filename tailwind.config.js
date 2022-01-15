d1c244934cf36d0146f96a9f5add4430a63f0e34
MadeReihan <reihanmade@gmail.com>
2022-01-15 15:07:22 +0700

    navbar and jumbotron

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,20 @@
 module.exports = {
     content: ["./public/*.html"],
     theme: {
-      extend: {},
+      extend: {
+        gradientColorStops:{
+          orange:'#F99500',
+          purple1:'#6c63ff',
+          puprle2: '#675ff4'
+        },
+        textColor:{
+          purple:'#6860f5',
+          red:'#fd4323'
+        },
+        borderColor:{
+          purple:'#6860f5',
+        },
+      },
     },
     plugins: [],
   }
